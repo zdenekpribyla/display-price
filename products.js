cba566fc415d6a94873625a10982bbd53bcc5f7c
Zdenek Pribyla <zdenek.pribyla@gmail.com>
2017-09-14 15:57:51 +0200

    add new js file for product objects

---
+++ products.js
@@ -0,0 +1,23 @@
+
+var products = [
+    {
+        'id': 1,
+        'product-name': 'Bike',
+        'color': 'red',
+        'price': 5800
+    },
+
+    {
+        'id': 2,
+        'product-name': 'Ball',
+        'color': 'red',
+        'price': 250
+    },
+
+    {
+        'id': 3,
+        'product-name': 'Boots',
+        'color': 'green',
+        'price': 2340
+    }
+];
